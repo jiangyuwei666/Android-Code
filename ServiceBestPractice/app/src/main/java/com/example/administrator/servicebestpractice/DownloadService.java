1cb4d796a4acf582b560b84ac666dff0055a4cdf
jiangyuwei666! <739843128@qq.com>
2017-10-10 22:04:09 +0800

    use service to download

--- ServiceBestPractice/app/src/main/java/com/example/administrator/servicebestpractice/DownloadService.java
+++ ServiceBestPractice/app/src/main/java/com/example/administrator/servicebestpractice/DownloadService.java
@@ -6,49 +6,109 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.v7.app.NotificationCompat;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class DownloadService extends Service {
 
     private DownloadTask downloadTask ;
     private String downloadUrl ;
+
+    public DownloadService() {
+    }//构造方法
+
     private DownloadListener listener = new DownloadListener() {
         @Override
         public void onProgress(int progress) {
             getNotificationManager().notify( 1 , getNotification( "Fucccck is downloading..." , progress )  );
-
-
         }
 
         @Override
         public void onSuccess() {
-
+            downloadTask = null ;
+            //下面的代码是下载成功时将前台服务通知关闭，并创建一个下载成功的通知
+            stopForeground( true ) ;//关闭上一个前台服务
+            getNotificationManager().notify( 1 , getNotification( "Download success" , -1 ) ) ;//调用notify()让通知显示出来
+            Toast.makeText( DownloadService.this , "Download success" , Toast.LENGTH_SHORT ).show();
         }
 
         @Override
         public void onFailed() {
-
+            downloadTask = null ;
+            //下载失败后先关闭前台服务通知，再创建一个下载失败的通知
+            stopForeground( true ) ;
+            getNotificationManager( ).notify( 1 , getNotification( "Download failed" , -1 ) ) ;
+            Toast.makeText( DownloadService.this , "Download failed" , Toast.LENGTH_SHORT ).show();
         }
 
         @Override
         public void onPaused() {
-
+            downloadTask = null ;
+            stopForeground( true ) ;
+            getNotificationManager( ).notify( 1, getNotification( "Download paused " , -1 ) ) ;
+            Toast.makeText( DownloadService.this , "Download paused " , Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onCanceled() {
-
+            downloadTask = null ;
+            stopForeground( true ) ;
+            getNotificationManager( ).notify( 1 , getNotification( "Download canceled" , -1 )  ) ;
+            Toast.makeText( DownloadService.this , "Download canceled"  , Toast.LENGTH_SHORT  ).show();
         }
     } ;
-    public DownloadService() {
-    }
+
+    private DownloadBinder mBinder = new DownloadBinder() ;
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder ;
     }
+
+    class DownloadBinder extends Binder {
+
+        public void startDownload ( String url ) {
+            if ( downloadTask == null ) {
+                downloadUrl = url ;
+                downloadTask = new DownloadTask( listener ) ;
+                downloadTask.execute( downloadUrl ) ;//执行一个异步任务,开始下载
+                startForeground( 1 , getNotification( "Downloading ... "  ,  0 ) );
+                Toast.makeText( DownloadService.this , "Fuck is Downloading ... " , Toast.LENGTH_SHORT ).show();
+            }
+        }
+        public void pauseDownload ( ) {
+            if ( downloadTask != null ) {
+                downloadTask.pauseDownload();
+            }
+        }
+        public void cancelDownload () {
+            if ( downloadTask != null ) {
+                downloadTask.cancelDownload();
+            }
+            else if ( downloadUrl != null ) {
+                String fileName = downloadUrl.substring( downloadUrl.lastIndexOf("/" ) ) ;
+                //lastIndexOf()返回的是资格字符串到最后这里的字节数，返回的是一个int类型的；然后substring()的参数是一个int型，是从int那个位置开始往后读取，就从你选择的地方又继续往下面读取
+                String directory = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_DOWNLOADS ).getPath() ;
+                //返回SD卡的目录，用于储存
+                File file = new File( directory + fileName ) ;
+                if ( file.exists() ) {
+                    file.delete() ;
+                }
+                getNotificationManager().cancel( 1 ) ;//？
+                stopForeground( true ) ;
+                Toast.makeText( DownloadService.this , "Canceled" , Toast.LENGTH_SHORT  ) .show();
+            }
+        }
+    }
+
+
+
 
     private NotificationManager getNotificationManager ( ) {
         return ( NotificationManager ) getSystemService( NOTIFICATION_SERVICE ) ;
@@ -62,9 +122,9 @@ public class DownloadService extends Service {
         builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
         builder.setContentTitle(title);
         builder.setContentIntent(pi);
-        if (progress > 0) {
+        if (progress >= 0) {
             builder.setContentText(progress + "%");
-            builder.setProgress(100, progress, false);
+            builder.setProgress(100, progress, false);//setProgress( 最大进度 , 已完成的进度 , 是否模糊进度条 )
         }
         return builder.build();
     }
--- ServiceBestPractice/app/src/main/java/com/example/administrator/servicebestpractice/MainActivity.java
+++ ServiceBestPractice/app/src/main/java/com/example/administrator/servicebestpractice/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.administrator.servicebestpractice;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private DownloadService.DownloadBinder downloadBinder ;//先创建一个用来绑定服务的DownloadBinder类实例,因为DownloadBinder是DownloadService中的匿名内部类,所以要这样创建
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = ( DownloadService.DownloadBinder ) service ;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button start = ( Button ) findViewById( R.id.start_download ) ;
+        Button pause = ( Button ) findViewById( R.id.pause_download ) ;
+        Button cancel = ( Button ) findViewById( R.id.cancel_download ) ;
+        start.setOnClickListener( this ) ;
+        pause.setOnClickListener( this ) ;
+        cancel.setOnClickListener( this ) ;
+        Intent intent = new Intent( this , DownloadService.class ) ;
+        startService( intent ) ;
+        bindService( intent , connection , BIND_AUTO_CREATE ) ;
+        if (ContextCompat.checkSelfPermission( MainActivity.this , Manifest.permission.WRITE_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( MainActivity.this , new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE } , 1 ) ;
+        }
+    }
+
+    @Override
+    public void onClick ( View v ) {
+        if ( downloadBinder == null ) {
+            return ;
+        }
+        switch ( v.getId() ) {
+            case R.id.start_download :
+                String url = "" ;
+                downloadBinder.startDownload( url ) ;
+                break;
+            case R.id.pause_download :
+                downloadBinder.pauseDownload();
+                break;
+            case R.id.cancel_download :
+                downloadBinder.cancelDownload();
+                break;
+            default:
+                break;
+        }
+    }
+
+    //因为要下载到本地的磁盘，所以必须要申请sd卡的权限
+    @Override
+    public void onRequestPermissionsResult( int requestCode , String [] permissions , int[] grantRequests ) {
+        switch ( requestCode ) {
+            case 1 :
+                if ( grantRequests.length > 0 && grantRequests[0] != PackageManager.PERMISSION_GRANTED ) {
+                    Toast.makeText( this , "你他吗别拒绝啊，拒绝了用nmh" , Toast.LENGTH_SHORT ).show() ;
+                    finish() ;//关闭应用
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy () {
+        super.onDestroy();
+        unbindService( connection ) ;//解绑。解绑绑定都用的ServiceConnetion对象
     }
 }
