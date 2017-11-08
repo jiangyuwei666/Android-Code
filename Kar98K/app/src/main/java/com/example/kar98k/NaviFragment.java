58345a4c9c62f641db22c81f9e8623baf9b871ae
jiangyuwei666! <739843128@qq.com>
2017-11-08 18:27:09 +0800

    asd

---
+++ Kar98K/app/src/main/java/com/example/kar98k/NaviFragment.java
@@ -0,0 +1,22 @@
+package com.example.kar98k;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/11/8.
+ */
+
+public class NaviFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate( R.layout.navi_fragment , container , false ) ;
+        return view ;
+    }
+}
