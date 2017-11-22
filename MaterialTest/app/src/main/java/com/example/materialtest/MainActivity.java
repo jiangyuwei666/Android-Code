2f3be2938e6d1d33df4b4f458da220be4fdba9a1
jiangyuwei666! <739843128@qq.com>
2017-11-22 21:10:10 +0800

    xx

--- MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.materialtest;
 
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = ( DrawerLayout ) findViewById( R.id.drawer_layout ) ;
         Toolbar toolbar = ( Toolbar ) findViewById( R.id.toolbar );
         setSupportActionBar( toolbar ) ;
-        //ActionBar
+        ActionBar actionBar = getSupportActionBar() ;
+        if ( actionBar != null ) {
+            actionBar.setDisplayHomeAsUpEnabled( true ) ;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch ( item.getItemId() ) {
+            case android.R.id.home :
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            default:
+                break;
+        }
+        return true ;
     }
 }
