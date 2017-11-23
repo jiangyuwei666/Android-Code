b9ede11d3b9dfca4981e38aba9d8de3259387d81
jiangyuwei666! <739843128@qq.com>
2017-11-23 21:17:02 +0800

    a

--- MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.materialtest;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -22,14 +24,24 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar() ;
         if ( actionBar != null ) {
             actionBar.setDisplayHomeAsUpEnabled( true ) ;
-        }
+        }//设置ActionBar
+        NavigationView navigationView = ( NavigationView) findViewById( R.id.nav_view) ;
+        //navigationView.setCheckedItem( R.id.nav_1 ) ;//默认点击的是这个玩意
+        //设置选项的监听
+        navigationView.setNavigationItemSelectedListener( new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                return true ;
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch ( item.getItemId() ) {
             case android.R.id.home :
-                drawerLayout.openDrawer(GravityCompat.START);
+                drawerLayout.openDrawer(GravityCompat.START);//设置ToolBar左边的那个按钮
                 break;
             default:
                 break;
